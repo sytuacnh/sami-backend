5eb6574ccb23e809b2fb7f275c152f24feb1140d
Sytuacnh <ylin@trinity.edu>
2022-06-04 14:32:38 -0700

    added gunicorn config

--- sami/settings.py
+++ sami/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SAMI_SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '104.198.233.196', 'mathinclude.org']
+ALLOWED_HOSTS = ['127.0.0.1', '104.198.233.196', '.mathinclude.org']
 
 AUTH_USER_MODEL = 'users.UserProfile'
 
