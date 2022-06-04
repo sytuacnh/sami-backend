46e6a02bff93128706c4ddad30ed07e46a9bb688
Sytuacnh <ylin@trinity.edu>
2022-06-04 11:05:57 -0700

    update allowed hosts

--- sami/settings.py
+++ sami/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SAMI_SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['104.198.233.196', 'mathinclude.org']
 
 AUTH_USER_MODEL = 'users.UserProfile'
 
