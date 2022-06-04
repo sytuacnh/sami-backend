5893b5a5c48c56399a095d2b8c87423bb6fbf95a
Sytuacnh <ylin@trinity.edu>
2022-06-04 11:18:51 -0700

    updated debug and hosts

--- sami/settings.py
+++ sami/settings.py
@@ -27,7 +27,7 @@ sys.path.insert(0, os.path.join(BASE_DIR, 'extra_apps'))
 SECRET_KEY = os.environ.get('SAMI_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['104.198.233.196']
 
