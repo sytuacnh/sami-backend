e7268e1eaa88c7825ab9eb93cb20c0bb735fb1b5
Sytuacnh <ylin@trinity.edu>
2022-06-11 20:32:41 -0700

    updated

--- apps/contactmessages/serializers.py
+++ apps/contactmessages/serializers.py
@@ -20,6 +20,7 @@ class ContactMessageSerializer(serializers.ModelSerializer):
             subject='{} Sent a Message from SaMi Website'.format(sender_name),
             message='[{0}] with email [{1}] said: \n\n{1}'.format(sender_name, email, content),
             recipient_list=SEND_TO_EMAILS,
+            from_email=email,
             fail_silently=False,
         ) 
 
